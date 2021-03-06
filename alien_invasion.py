f5c295eae89069c27a21c3e03225bf5693fcc175
Bryan Hand <bryan_hand1@baylor.edu>
2021-03-06 16:58:12 -0600

    Continuous movement right

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -41,8 +42,12 @@ class AlienInvasion:
             # Piloting the ship responding to keypress
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
                     # move the ship right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop
