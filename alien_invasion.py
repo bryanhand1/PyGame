63d440d1a9ed68bb6e64c02048bce601565952db
Bryan Hand <bryan_hand1@baylor.edu>
2021-03-07 16:41:00 -0600

    Refactoring create fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -115,11 +115,15 @@ class AlienInvasion:
 
         # Creat the first row of aliens
         for alien_number in range(number_aliens_x):
-            # Create and alien and place it in the row
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        # Create and alien and place it in the row
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
 
 if __name__ == "__main__":
